50d0af54e64bb754d17170acaede5f68ddca40b3
yzma <yma8866@yahoo.com>
2018-05-09 14:31:46 -0500

    fix persistence for uninitialized zk-atom

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yzma/avout "0.5.5"
+(defproject yzma/avout "0.5.6-SNAPSHOT"
   :description "Avout: Distributed State in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [zookeeper-clj "0.9.3"]])
